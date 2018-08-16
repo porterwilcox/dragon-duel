b3985edc112206841dde565e21776726a9e10db3
Porter Wilcox <wilcox.porter@gmail.com>
2018-08-16 11:50:17 -0600

    fix delay on initial attack onclick

--- app/components/duelService.js
+++ app/components/duelService.js
@@ -69,7 +69,7 @@ export default class DuelService {
         })
             .then(res => {
                 game = res.data
+                callback(game)
             })
-            .then(callback(game))
     }
 }
